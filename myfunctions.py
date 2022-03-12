bdf174ec1985225301390b47322cf09bb2822833
NMIXXModTeam <101436400+NMIXXModTeam@users.noreply.github.com>
2022-03-12 03:59:55 +0100

    Update myfunctions.py

--- myfunctions.py
+++ myfunctions.py
@@ -2,6 +2,18 @@ import praw
 import pandas as pd
 
 def hello():
+  # Define user agent
+  user_agent = "praw_scraper_1.0"
+
+  # Create an instance of reddit class
+  reddit = praw.Reddit(client_id="yxI4dSUvZzWGpuqHEl4DqA",
+                       client_secret="UDReCN0uxUF_56oAMGfmMTugmFPWbA",
+                       user_agent=user_agent
+  )
+
+  # Create sub-reddit instance
+  subreddit_name = "nmixx"
+  subreddit = reddit.subreddit(subreddit_name)
   return "Hello World"
 
 def recap():  
