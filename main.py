6348f513c636780527a644a76004fac68dba19ad
NMIXXModTeam <101436400+NMIXXModTeam@users.noreply.github.com>
2022-03-12 04:19:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,13 @@ client = discord.Client()
 async def on_ready():
   print("We have loggied in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith("$recap"):
-    await message.channel.send(recap())
     
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith("$hello"):
+  if message.content.startswith("$recap"):
     hello()
     with open('weeklyrecap.txt', 'rb') as fp:
       await message.channel.send(file=discord.File(fp, 'weeklyrecap.txt'))
