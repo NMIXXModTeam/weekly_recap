eb64611a5caa847e62496c15b634aca771350154
NMIXXModTeam <101436400+NMIXXModTeam@users.noreply.github.com>
2022-03-12 07:49:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ async def on_message(message):
       await message.channel.send(file=discord.File(fp, 'weeklyrecap.txt'))
 #    await message.channel.send(recap())
     
-client.run('OTUxOTg5NDAxMTY3MjMzMTE2.YivfOA.Eu4RpqzDw5qJ8b-wkx5U_MhLw1U')
+client.run(process.env.TOKEN)
