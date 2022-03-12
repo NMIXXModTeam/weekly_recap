016cf788a8d55e7bafe22c5fa231d4bd3ade7720
NMIXXModTeam <101436400+NMIXXModTeam@users.noreply.github.com>
2022-03-12 03:32:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,39 @@ from discord import Embed
 from discord.ext import commands
 import asyncpraw
 import random
+import discord
+from myfunctions import weekly_recap, hello
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("We have loggied in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("$weeklyrecap"):
+    await message.channel.send(weekly_recap())
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("$hello"):
+    await message.channel.send(hello())
 
 @commands.command(name="meme")
 async def meme(self, ctx, subred="memes"): # default subreddit is memes, later in the command you can select one of your choice (example: !meme python --> chooses r/python reddit post)
     msg = await ctx.send('Loading ... ')
 
-    reddit = asyncpraw.Reddit(client_id='yxI4dSUvZzWGpuqHEl4DqA',
-                              client_secret='UDReCN0uxUF_56oAMGfmMTugmFPWbA',
-                              user_agent=user_agent)
+    reddit = asyncpraw.Reddit(client_id='clientid',
+                              client_secret='clientsecret',
+                              username='username',
+                              password='password',
+                              user_agent='useragent')
 
     subreddit = await reddit.subreddit(subred)
     all_subs = []
@@ -32,3 +57,4 @@ async def meme(self, ctx, subred="memes"): # default subreddit is memes, later i
     await ctx.send(embed=embed)
     await msg.edit(content=f'<https://reddit.com/r/{subreddit}/> :white_check_mark:') # < and > remove the embed link
     return
+client.run('OTUxOTg5NDAxMTY3MjMzMTE2.YivfOA.Eu4RpqzDw5qJ8b-wkx5U_MhLw1U')
